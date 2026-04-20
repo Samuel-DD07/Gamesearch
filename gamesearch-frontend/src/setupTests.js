aed3580d15d9d42cdf50ec6d38537c44e1b9eecf
Samuel Dorismond <samuel.dorismond@epita.fr>
2026-04-20 18:05:32 +0000

    Revert "Merge branch 'tests' into 'dev'"
    This reverts merge request !12

--- gamesearch-frontend/src/setupTests.js
+++
@@ -1,5 +0,0 @@
-// jest-dom adds custom jest matchers for asserting on DOM nodes.
-import '@testing-library/jest-dom';
-
-// Mock window.scrollTo to prevent jsdom errors with framer-motion
-window.scrollTo = jest.fn();
--- gamesearch/src/test/java/fr/epita/apping/fullstack/gamesearch/GamesearchApplicationTests.java
+++ gamesearch/src/test/java/fr/epita/apping/fullstack/gamesearch/GamesearchApplicationTests.java
@@ -1,18 +1,10 @@
 package fr.epita.apping.fullstack.gamesearch;
 
-import fr.epita.apping.fullstack.gamesearch.kafka.producer.GameIngestionProducer;
-import fr.epita.apping.fullstack.gamesearch.kafka.producer.GameIngestionStatusProducer;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
 class GamesearchApplicationTests {
-
-  @MockBean private GameIngestionProducer gameIngestionProducer;
-  @MockBean private GameIngestionStatusProducer gameIngestionStatusProducer;
 
   @Test
   void contextLoads() {}
